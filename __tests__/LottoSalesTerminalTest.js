348f7608930ce69b02363e042d471022ce4e6fc4
HareuBang <divjae7124@gmail.com>
2025-10-29 20:14:05 +0900

    test: publishLottos 구입 금액만큼의 로또 발행하는 기능 테스트 코드 추가

--- __tests__/LottoSalesTerminalTest.js
+++ __tests__/LottoSalesTerminalTest.js
@@ -1,8 +1,42 @@
 import LottoSalesTerminal from "../src/LottoSalesTerminal.js";
 import { AMOUNT_ERROR } from "../src/constants/errorMessage.js";
+import { LOTTO_PRICE } from "../src/constants/constants.js";
 
 describe("로또 판매 단말기 테스트", () => {
-  describe("publishLottos 기능 테스트", () => {});
+  describe("publishLottos 기능 테스트", () => {
+    let lottoSalesTerminal;
+
+    beforeEach(() => {
+      // 정렬되지 않은 로또 번호 배열들
+      const mockRandomPickUniqueNumber = jest
+        .fn()
+        .mockReturnValueOnce([23, 41, 43, 21, 8, 42])
+        .mockReturnValueOnce([5, 3, 16, 11, 38, 32])
+        .mockReturnValueOnce([44, 36, 35, 16, 11, 7]);
+
+      lottoSalesTerminal = new LottoSalesTerminal({
+        randomPickUniqueNumber: mockRandomPickUniqueNumber,
+      });
+    });
+
+    test.each([
+      [
+        "3000",
+        3,
+        [
+          [8, 21, 23, 41, 42, 43],
+          [3, 5, 11, 16, 32, 38],
+          [7, 11, 16, 35, 36, 44],
+        ],
+      ],
+      ["1000", 1, [[8, 21, 23, 41, 42, 43]]],
+    ])(
+      `로또 구입 금액 "%s" 원을 입력하면 로또 가격(${LOTTO_PRICE})으로 나눈 %d 수량의 오름차순으로 정렬된 로또를 발행한다`,
+      (amount, _, expected) => {
+        expect(lottoSalesTerminal.publishLottos(amount)).toEqual(expected);
+      }
+    );
+  });
 
   describe("publishLottos 예외 테스트", () => {
     const lottoSalesTerminal = new LottoSalesTerminal();
