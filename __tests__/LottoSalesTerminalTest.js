f141a13c7e0afaf7a2b9b7ec5dd8a9f5b3f9db4a
HareuBang <divjae7124@gmail.com>
2025-11-02 00:47:08 +0900

    test: LottoSalesTerminal 구조 변경으로 인한 테스트 코드 수정
    - LottoSalesTerminal 클래스의 LottoTicketFactory 의존성 추가, 입력값 검증
     책임 분리의 구조 변경에 맞춰 테스트 코드를 수정했습니다.

--- __tests__/LottoSalesTerminalTest.js
+++ __tests__/LottoSalesTerminalTest.js
@@ -1,5 +1,5 @@
 import LottoSalesTerminal from "../src/LottoSalesTerminal.js";
-import { AMOUNT_ERROR } from "../src/constants/errorMessage.js";
+import { AMOUNT_NOT_MULTIPLE_OF_PRICE_ERROR } from "../src/constants/errorMessage.js";
 import { LOTTO } from "../src/constants/constants.js";
 
 describe("로또 판매 단말기 테스트", () => {
@@ -15,9 +15,16 @@ describe("로또 판매 단말기 테스트", () => {
       .mockReturnValueOnce([5, 3, 16, 11, 38, 32])
       .mockReturnValueOnce([44, 36, 35, 16, 11, 7]);
 
+    const mockLottoTicketFactory = jest.fn(({ amount, quantity, lottos }) => ({
+      amount,
+      quantity,
+      lottos,
+    }));
+
     lottoSalesTerminal = new LottoSalesTerminal({
       onRandomPickUniqueNumber: mockRandomPickUniqueNumber,
       onLottoFactory: mockLottoFactory,
+      onLottoTicketFactory: mockLottoTicketFactory,
     });
   });
 
@@ -37,6 +44,7 @@ describe("로또 판매 단말기 테스트", () => {
       `로또 구입 금액 "%s" 원을 입력하면 로또 가격(${LOTTO.PRICE})으로 나눈 %d 수량의 오름차순으로 정렬된 로또를 발행한다`,
       (amount, quantity, lottos) => {
         expect(lottoSalesTerminal.publishLottos(amount)).toEqual({
+          amount,
           quantity,
           lottos,
         });
@@ -45,38 +53,11 @@ describe("로또 판매 단말기 테스트", () => {
   });
 
   describe("publishLottos 예외 테스트", () => {
-    test.each([[""], ["   "]])(
-      `구입 금액이 빈 문자열 또는 공백일 경우 "${AMOUNT_ERROR.INPUT_EMPTY}" 에러를 반환합니다. (입력: '%s')`,
-      (amount) => {
-        expect(() => lottoSalesTerminal.publishLottos(amount)).toThrow(
-          AMOUNT_ERROR.INPUT_EMPTY
-        );
-      }
-    );
-
-    test.each([["amount"], ["1500A0"], ["1000A"]])(
-      `구입 금액이 숫자가 아닐 경우 "${AMOUNT_ERROR.NOT_NUMBER}" 에러를 반환합니다. (입력: '%s')`,
-      (amount) => {
-        expect(() => lottoSalesTerminal.publishLottos(amount)).toThrow(
-          AMOUNT_ERROR.NOT_NUMBER
-        );
-      }
-    );
-
-    test.each([["0"], ["-1"], ["-1000"]])(
-      `구입 금액이 0 이하일 경우 "${AMOUNT_ERROR.NOT_POSITIVE_NUMBER}" 에러를 반환합니다. (입력: '%s')`,
-      (amount) => {
-        expect(() => lottoSalesTerminal.publishLottos(amount)).toThrow(
-          AMOUNT_ERROR.NOT_POSITIVE_NUMBER
-        );
-      }
-    );
-
     test.each([["1"], ["100"], ["999"], ["1001"], ["25400"]])(
-      `구입 금액이 1000원 단위가 아닐 경우 "${AMOUNT_ERROR.NOT_MULTIPLE_OF_PRICE}" 에러를 반환합니다. (입력: '%s')`,
+      `구입 금액이 1000원 단위가 아닐 경우 "${AMOUNT_NOT_MULTIPLE_OF_PRICE_ERROR}" 에러를 반환합니다. (입력: '%s')`,
       (amount) => {
         expect(() => lottoSalesTerminal.publishLottos(amount)).toThrow(
-          AMOUNT_ERROR.NOT_MULTIPLE_OF_PRICE
+          AMOUNT_NOT_MULTIPLE_OF_PRICE_ERROR
         );
       }
     );
