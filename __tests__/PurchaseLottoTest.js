df79d1100bccfb337081101326e05921ae48bf3d
HareuBang <divjae7124@gmail.com>
2025-11-02 04:44:28 +0900

    test: 사용자가 금액을 입력했을 때 로또 발행 및 출력 흐름 테스트 작성

---
+++ __tests__/PurchaseLottoTest.js
@@ -0,0 +1,41 @@
+import PurchaseLotto from "../src/controller/PurchaseLotto.js";
+
+test("사용자가 금액을 입력하면 로또 발행 및 출력이 정상적으로 동작한다", () => {
+  const amount = 2000;
+  const quantity = 2;
+  const lottos = [
+    [7, 11, 30, 40, 42, 43],
+    [2, 13, 22, 32, 38, 45],
+  ];
+
+  const mockInputView = { handlePurchaseAmount: jest.fn(() => amount) };
+  const mockLottoSalesTerminal = {
+    publishLottos: jest.fn(() => ({
+      quantity,
+      lottos,
+    })),
+  };
+  const mockOutputView = { renderLottoTicket: jest.fn() };
+
+  const purchaseLotto = new PurchaseLotto({
+    inputview: mockInputView,
+    lottoSalesTerminal: mockLottoSalesTerminal,
+    outputView: mockOutputView,
+  });
+
+  purchaseLotto.run();
+
+  // InputView
+  expect(mockInputView.handlePurchaseAmount).toHaveBeenCalledTimes(1);
+
+  // LottoSalesTerminal
+  expect(mockLottoSalesTerminal.publishLottos).toHaveBeenCalledTimes(1);
+  expect(mockLottoSalesTerminal.publishLottos).toHaveBeenCalledWith(amount);
+
+  // OutputView
+  expect(mockOutputView.renderLottoTicket).toHaveBeenCalledTimes(1);
+  expect(mockOutputView.renderLottoTicket).toHaveBeenCalledWith({
+    quantity,
+    lottos,
+  });
+});
