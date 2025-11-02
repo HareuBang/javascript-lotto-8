21b5576c37ed76721e36e9e6b39b88668bff1bc7
HareuBang <divjae7124@gmail.com>
2025-11-02 20:32:44 +0900

    test: handleWinningLotto 당첨 번호를 입력받는 기능 테스트 추가

--- __tests__/InputViewTest.js
+++ __tests__/InputViewTest.js
@@ -1,5 +1,8 @@
 import InputView from "../src/console/InputView.js";
-import { NUMBER_ERROR } from "../src/constants/errorMessage.js";
+import {
+  INPUT_EMPTY_ERROR,
+  NUMBER_ERROR,
+} from "../src/constants/errorMessage.js";
 
 describe("콘솔 입력 클래스 테스트", () => {
   let inputView;
@@ -11,7 +14,7 @@ describe("콘솔 입력 클래스 테스트", () => {
   });
 
   describe("handlePurchaseAmount 기능 테스트", () => {
-    test("올바른 입력값을 입력하면 숫자로 변환하여 반환합니다.", async () => {
+    test("구입할 금액을 입력하면 숫자로 변환하여 반환합니다.", async () => {
       mockReadLineAsync.mockResolvedValue("3000");
 
       const result = await inputView.handlePurchaseAmount();
@@ -19,14 +22,23 @@ describe("콘솔 입력 클래스 테스트", () => {
     });
   });
 
+  describe("handleWinningLotto 기능 테스트", () => {
+    test("당첨 번호를 입력하면 ,(쉼표)를 기준으로 분리된 숫자 배열을 반환합니다.", async () => {
+      mockReadLineAsync.mockResolvedValue("7,6,4,11,32,24");
+
+      const result = await inputView.handleWinningLotto();
+      expect(result).toEqual([7, 6, 4, 11, 32, 24]);
+    });
+  });
+
   describe("handlePurchaseAmount 예외 테스트", () => {
     test.each([[""], ["   "]])(
-      `입력값이 빈 문자열 또는 공백일 경우 "${NUMBER_ERROR.INPUT_EMPTY}" 에러를 반환합니다.`,
+      `입력값이 빈 문자열 또는 공백일 경우 "${INPUT_EMPTY_ERROR}" 에러를 반환합니다.`,
       async (amount) => {
         mockReadLineAsync.mockResolvedValue(amount);
 
         await expect(inputView.handlePurchaseAmount()).rejects.toThrow(
-          NUMBER_ERROR.INPUT_EMPTY
+          INPUT_EMPTY_ERROR
         );
       }
     );
@@ -59,6 +71,50 @@ describe("콘솔 입력 클래스 테스트", () => {
         mockReadLineAsync.mockResolvedValue(amount);
 
         await expect(inputView.handlePurchaseAmount()).rejects.toThrow(
+          NUMBER_ERROR.NOT_POSITIVE_NUMBER
+        );
+      }
+    );
+  });
+
+  describe("handleWinningLotto 예외 테스트", () => {
+    test.each([[""], ["   "]])(
+      `입력값이 빈 문자열 또는 공백일 경우 "${INPUT_EMPTY_ERROR}" 에러를 반환합니다. (입력: '%s')`,
+      async (amount) => {
+        mockReadLineAsync.mockResolvedValue(amount);
+
+        await expect(inputView.handleWinningLotto()).rejects.toThrow(
+          INPUT_EMPTY_ERROR
+        );
+      }
+    );
+
+    test(`입력값을 ,(쉼표)로 분리 후 요소들이 숫자가 아닐 경우 "${NUMBER_ERROR.NOT_NUMBER}" 에러를 반환합니다.`, async () => {
+      const winningLotto = "1,2,A,4,5,6";
+
+      mockReadLineAsync.mockResolvedValue(winningLotto);
+
+      await expect(inputView.handleWinningLotto()).rejects.toThrow(
+        NUMBER_ERROR.NOT_NUMBER
+      );
+    });
+
+    test(`입력값을 ,(쉼표)로 분리 후 요소들이 숫자가 아닐 경우 "${NUMBER_ERROR.NOT_INTEGER}" 에러를 반환합니다.`, async () => {
+      const winningLotto = "1,2,3.14,4,5,6";
+
+      mockReadLineAsync.mockResolvedValue(winningLotto);
+
+      await expect(inputView.handleWinningLotto()).rejects.toThrow(
+        NUMBER_ERROR.NOT_INTEGER
+      );
+    });
+
+    test.each([["1,2,3,0,5,6"], ["7,45,-1,3,9,1"]])(
+      `입력값을 ,(쉼표)로 분리 후 요소들이 0 이하일 경우 "${NUMBER_ERROR.NOT_POSITIVE_NUMBER}" 에러를 반환합니다. (입력: '%s')`,
+      async (winningLotto) => {
+        mockReadLineAsync.mockResolvedValue(winningLotto);
+
+        await expect(inputView.handleWinningLotto()).rejects.toThrow(
           NUMBER_ERROR.NOT_POSITIVE_NUMBER
         );
       }
