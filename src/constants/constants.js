dd638bf744bfb83e5df5963822f675e25332d4fb
HareuBang <divjae7124@gmail.com>
2025-11-03 04:05:59 +0900

    feat: 구매한 로또 번호와 당첨 번호 비교 기능 구현
    - WinningLottoChecker는 순수하게 비교 역할만 수행하기 때문에 lottoTicket,
     winningLotto, bonusNumber는 생성자가 아닌 메서드 인자로 전달하도록 설계했
    습니다.
    - 추후 당첨 내역 기록 기능이 추가될 가능성을 고려해 클래스로 구현했습니다.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,3 +4,11 @@ export const LOTTO = {
   MAX_NUMBER: 45,
   COUNT: 6,
 };
+
+export const WINNING_CRITERIA = [
+  { matchCount: 6, isBonus: false, prize: 2_000_000_000 },
+  { matchCount: 5, isBonus: true, prize: 30_000_000 },
+  { matchCount: 5, isBonus: false, prize: 1_500_000 },
+  { matchCount: 4, isBonus: false, prize: 50_000 },
+  { matchCount: 3, isBonus: false, prize: 5_000 },
+];
---
+++ src/model/WinningLottoChecker.js
@@ -0,0 +1,37 @@
+import { WINNING_CRITERIA } from "../constants/constants.js";
+
+class WinningLottoChecker {
+  #matchLottos(lottoTicket, winningLotto, bonusNumber) {
+    const lottos = lottoTicket.getLottos();
+    const winningLottoNumbers = new Set(winningLotto.getNumbers());
+    const winningResult = WINNING_CRITERIA.map((criteria) => ({
+      ...criteria,
+      count: 0,
+    }));
+
+    lottos.forEach((lotto) => {
+      const lottoNumbers = lotto.getNumbers();
+
+      const matchCount = lottoNumbers.filter((number) =>
+        winningLottoNumbers.has(number)
+      ).length;
+
+      let isBonus = false;
+      if (matchCount === 5) isBonus = lottoNumbers.includes(bonusNumber);
+
+      const matchCriteria = winningResult.find(
+        (match) => match.matchCount === matchCount && match.isBonus === isBonus
+      );
+
+      if (matchCriteria) matchCriteria.count++;
+    });
+
+    return winningResult;
+  }
+
+  evaluateWinning(lottoTicket, winningLotto, bonusNumber) {
+    return this.#matchLottos(lottoTicket, winningLotto, bonusNumber);
+  }
+}
+
+export default WinningLottoChecker;
