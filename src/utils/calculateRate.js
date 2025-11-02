38cc56d4926c3dbbc5feabd154a83e979be6ebf0
HareuBang <divjae7124@gmail.com>
2025-11-03 04:15:01 +0900

    feat: 당첨 내역을 기준으로 수익률을 계산하는 기능 구현

---
+++ src/utils/calculateRate.js
@@ -0,0 +1,13 @@
+const calculateRate = (lottoTicket, winningResult) => {
+  const amount = lottoTicket.getAmount();
+  const winning = winningResult.filter((result) => result.count > 0);
+
+  const totalWinningAmount = winning.reduce(
+    (result, { prize, count }) => result + prize * count,
+    0
+  );
+
+  const rate = (totalWinningAmount / amount) * 100;
+
+  return rate.toFixed(2);
+};
