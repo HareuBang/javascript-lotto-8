0c28ec9c7f1f4ea1812ad853c3fb8cedc1c395aa
HareuBang <divjae7124@gmail.com>
2025-11-03 13:13:39 +0900

    test: 당첨 내역 출력 기능 테스트 작성

--- __tests__/OutputViewTest.js
+++ __tests__/OutputViewTest.js
@@ -53,4 +53,32 @@ describe("콘솔 출력 클래스 테스트", () => {
       expect(mockPrint).toHaveBeenCalledWith(expected);
     });
   });
+
+  describe("renderWinningDetails 기능 테스트", () => {
+    let outputView;
+    let mockPrint;
+
+    beforeEach(() => {
+      mockPrint = jest.fn();
+      outputView = new OutputView(mockPrint);
+    });
+
+    test("당첨 내역을 정상적으로 출력한다.", () => {
+      const winningDetails = [
+        { matchCount: 3, isBonus: false, prize: 5_000, count: 3 },
+        { matchCount: 4, isBonus: false, prize: 50_000, count: 0 },
+        { matchCount: 5, isBonus: false, prize: 1_500_000, count: 0 },
+        { matchCount: 5, isBonus: true, prize: 30_000_000, count: 1 },
+        { matchCount: 6, isBonus: false, prize: 2_000_000_000, count: 1 },
+      ];
+
+      const expected =
+        "3개 일치 (5,000원) - 3개\n4개 일치 (50,000원) - 0개\n5개 일치 (1,500,000원) - 0개\n5개 일치, 보너스 볼 일치 (30,000,000원) - 1개\n6개 일치 (2,000,000,000원) - 1개";
+
+      outputView.renderWinningDetails(winningDetails);
+
+      expect(mockPrint).toHaveBeenCalledTimes(1);
+      expect(mockPrint).toHaveBeenCalledWith(expected);
+    });
+  });
 });
