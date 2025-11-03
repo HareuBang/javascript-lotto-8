66d50d8d2be64971501e97d53992b244ae8772c9
HareuBang <divjae7124@gmail.com>
2025-11-03 21:58:41 +0900

    test: OutputView 출력 형식 수정으로 OutputViewTest도 기대값 변경

--- __tests__/OutputViewTest.js
+++ __tests__/OutputViewTest.js
@@ -45,7 +45,7 @@ describe("콘솔 출력 클래스 테스트", () => {
       const mockLottoTicket = new MockLottoTicket(quantity, lottos);
 
       const expected =
-        "3개를 구매했습니다.\n[8, 21, 23, 41, 42, 43]\n[3, 5, 11, 16, 32, 38]\n[7, 11, 16, 35, 36, 44]";
+        "\n3개를 구매했습니다.\n[8, 21, 23, 41, 42, 43]\n[3, 5, 11, 16, 32, 38]\n[7, 11, 16, 35, 36, 44]\n";
 
       outputView.renderLottoTicket(mockLottoTicket);
 
@@ -73,7 +73,7 @@ describe("콘솔 출력 클래스 테스트", () => {
       ];
 
       const expected =
-        "3개 일치 (5,000원) - 3개\n4개 일치 (50,000원) - 0개\n5개 일치 (1,500,000원) - 0개\n5개 일치, 보너스 볼 일치 (30,000,000원) - 1개\n6개 일치 (2,000,000,000원) - 1개";
+        "\n당첨 통계\n---\n3개 일치 (5,000원) - 3개\n4개 일치 (50,000원) - 0개\n5개 일치 (1,500,000원) - 0개\n5개 일치, 보너스 볼 일치 (30,000,000원) - 1개\n6개 일치 (2,000,000,000원) - 1개";
 
       outputView.renderWinningDetails(winningDetails);
 
